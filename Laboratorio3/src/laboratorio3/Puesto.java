b44fd717ecd38cee607c0873722aa45cec84fc04
santiago-1803740 <u1803740@unimilitar.edu.co>
2020-04-15 20:49:42 -0500

    puesto

--- Laboratorio3/src/laboratorio3/Puesto.java
+++ Laboratorio3/src/laboratorio3/Puesto.java
@@ -10,7 +10,8 @@ public class Puesto {
     private int numero;
     private String estado;
     private Carro carro;
-    private moto moto;
+    private Moto moto;
+    private Bicicleta bicicleta;
     
     public Puesto(int numero){
         this.numero = numero;
@@ -20,7 +21,12 @@ public class Puesto {
     public void asignarPuesto(Carro carro){
         this.carro = carro;
     }
-
+    public void asignarPuesto(Moto moto){
+        this.moto = moto;
+    }
+    public void asignarPuesto(Bicicleta bicicleta){
+        this.bicicleta = bicicleta;
+    }
     public int getNumero() {
         return numero;
     }
@@ -40,11 +46,21 @@ public class Puesto {
     public Carro getCarro() {
         return carro;
     }
-
+    public Moto getMoto() {
+        return moto;
+    }
+    public Bicicleta getBicicleta() {
+        return bicicleta;
+    }
     public void setCarro(Carro carro) {
         this.carro = carro;
     }
-    
+    public void setMoto(Moto moto) {
+        this.moto = moto;
+    }
+    public void setBicicleta(Bicicleta bicicleta) {
+        this.bicicleta = bicicleta;
+    }    
     public String libreString(){
         return "Puesto: "+this.getNumero()
             +"\nEstado: "+this.getEstado();
@@ -54,6 +70,9 @@ public class Puesto {
         return "Puesto: "+this.getNumero()
             +"\nEstado: "+this.getEstado()
             +"\nPlaca del carro: "+this.getCarro().getPlaca()
+            +"\nPlaca del carro: "+this.getMoto().getPlaca()
             +"\nHora: "+this.getCarro().getHora()+":"+this.getCarro().getMinuto();
+            +"\nHora: "+this.getCarro().getHora()+":"+this.getMoto().getMinuto();
+            +"\nHora: "+this.getCarro().getHora()+":"+this.getBicicleta().getMinuto();
     }
 }
